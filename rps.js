5ce91d397abf61bb7cea088bad411f60a0f148a1
ansonm569 <108905262+ansonm569@users.noreply.github.com>
2023-01-26 10:30:08 -0500

    Add random computer selector

--- rps.js
+++ rps.js
@@ -7,6 +7,15 @@
 //     Make sure player selection is case-insensitive 
 
 
+function getComputerChoice() {
+    //random selection
+    var rps = ['Rock', 'Paper', 'Scissors'];
+    let i = Math.floor(Math.random()*3);
+    let compSelection = rps[i];
+    console.log(compSelection);
+    return compSelection;
+}
 
+getComputerChoice();
 
 //more elsewhere
