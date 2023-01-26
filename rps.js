2052611ebd47f51bf166465220625e32a7951803
ansonm569 <108905262+ansonm569@users.noreply.github.com>
2023-01-26 10:22:54 -0500

    Add core files

---
+++ rps.js
@@ -0,0 +1,12 @@
+
+
+// create function called "GetComputerChoice" that randomly selects rock paper or scissors
+// write a function that plays a single round or RPS
+//     Function should should take playerSelection and computerSelection as parameters
+//     Return stringg that delcares the winner / outcome
+//     Make sure player selection is case-insensitive 
+
+
+
+
+//more elsewhere
