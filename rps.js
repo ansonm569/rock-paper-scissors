96dd43738e0180dcebcba8bf2a31c387c38f777d
ansonm569 <108905262+ansonm569@users.noreply.github.com>
2023-01-27 11:00:39 -0500

    Add score trackers

--- rps.js
+++ rps.js
@@ -6,33 +6,52 @@
 //     Return stringg that delcares the winner / outcome
 //     Make sure player selection is case-insensitive 
 
+const win = "You win!";
+const lose = "You lose!";
+const tie = "Tie game!";
 
 function getComputerChoice() {
     // computer random selection and return
     var rps = ['rock', 'paper', 'scissors'];
     let i = Math.floor(Math.random()*3);
-    console.log(rps[i]);
+    // console.log(rps[i]);
     return rps[i];
 }
 
 function playRound(playerSelection, computerSelection) {
-    // take two inputs and determine victor or tie
+    // Cleans players selection
     let pClean = (playerSelection.toLowerCase());
     if (pClean == computerSelection) {
-        return ("Tie game!");
+        return (tie);
+        // If it's a tie, declare tie game
     }
     else if ((pClean == "rock" && computerSelection == "scissors") || 
     (pClean == "paper" && computerSelection == "rock") || (pClean == "scissors" && computerSelection == "paper"))  {
-        return (`You win! ${pClean.charAt(0).toUpperCase() + pClean.slice(1)} beats ${computerSelection}.`);
+        return (win);
+        // return ('${pClean.charAt(0).toUpperCase() + pClean.slice(1)} beats ${computerSelection}.`);
+        // If there is a scenario where the player wins, return a statement declaring victory and explain why
     }
     else {
-        return (`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${pClean}.`)
+        return (lose); 
+        // return (`${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${pClean}.`)
+        // If there is a scenario with computer wins, return a statement declaring a loss and explain why
     }
     }
 
 const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-console.log(playRound(playerSelection, computerSelection));
+
+for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Rock, paper, or scissors?");
+    let computerScoreTracker = 0;
+    let playerScoreTracker = 0;
+    if (playRound(playerSelection, computerSelection) == win) {
+        computerScoreTracker++;
+    }
+    else if (playRound(playerSelection, computerSelection) == lose) {
+        playerScoreTracker--
+    }
+
+}
 
 
 //more elsewhere
